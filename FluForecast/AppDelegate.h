501fc8456f73d5e487a4012e16a4877e18cb9ec4
Changkun Zhao <changkunzhao@client-130-203-188-3.mobility-up.psu.edu>
2014-07-21 16:26:42 -0400

    Initial Commit

---
+++ FluForecast/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FluForecast
+//
+//  Created by Changkun Zhao on 7/21/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
