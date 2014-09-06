0a3b869c24691c3b1320a5ec90c5df72c8d916ae
Changkun Zhao <changkunzhao@Changkuns-MacBook-Air.local>
2014-09-05 22:07:04 -0400

    modified side bars

---
+++ FluForecast/SideElements/SideTitleViewController.h
@@ -0,0 +1,13 @@
+//
+//  SideTitleViewController.h
+//  FluForecast
+//
+//  Created by Changkun Zhao on 9/5/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideTitleViewController : UIViewController
+
+@end
---
+++ FluForecast/SideElements/SideTitleViewController.m
@@ -0,0 +1,49 @@
+//
+//  SideTitleViewController.m
+//  FluForecast
+//
+//  Created by Changkun Zhao on 9/5/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import "SideTitleViewController.h"
+
+@interface SideTitleViewController ()
+
+@end
+
+@implementation SideTitleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
