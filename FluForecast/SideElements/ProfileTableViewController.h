f9196464de890ac3b6ab7bf3fb95dced7f7c6c1e
Changkun Zhao <changkunzhao@Changkuns-MacBook-Air.local>
2014-09-07 22:52:21 -0400

    fixed profile editing page

---
+++ FluForecast/SideElements/ProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileTableViewController.h
+//  FluForecast
+//
+//  Created by Changkun Zhao on 9/7/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileTableViewController : UITableViewController
+
+@end
