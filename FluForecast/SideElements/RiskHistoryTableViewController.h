309d8e721ff5f1955b8d6b4fcc3f865599a93e65
Changkun Zhao <changkunzhao@Changkuns-MacBook-Air.local>
2014-09-10 17:04:16 -0400

    change linechart interface.

---
+++ FluForecast/SideElements/RiskHistoryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RiskHistoryTableViewController.h
+//  FluForecast
+//
+//  Created by Changkun Zhao on 9/10/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RiskHistoryTableViewController : UITableViewController
+
+@end
---
+++ FluForecast/SideElements/SettingTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingTableViewController.h
+//  FluForecast
+//
+//  Created by Changkun Zhao on 9/7/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingTableViewController : UITableViewController
+
+@end
