0b246a172d99a879adff0f1b0d9c9dc269da732b
Changkun Zhao <changkunzhao@Changkuns-MacBook-Air.local>
2014-10-07 00:33:08 -0400

    fixed to run under ios8

---
+++ FluForecast/ActionPages/ActivityHub.m
@@ -0,0 +1,21 @@
+//
+//  ActivityHub.m
+//  HHeal
+//
+//  Created by Changkun Zhao on 10/6/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import "ActivityHub.h"
+
+@implementation ActivityHub
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ FluForecast/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  HHeal
+//
+//  Created by Changkun Zhao on 10/6/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UINavigationController
+
+@end
