e0f7d416334f5b9279019e85b8446b4a81fecc64
Changkun Zhao <changkunzhao@Changkuns-MacBook-Air.local>
2014-10-12 17:24:25 -0400

    added sidebar about Ebola

---
+++ FluForecast/SideElements/EbolaRiskViewController.h
@@ -0,0 +1,13 @@
+//
+//  EbolaRiskViewController.h
+//  HHeal
+//
+//  Created by Changkun Zhao on 10/12/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EbolaRiskViewController : UIViewController
+
+@end
---
+++ FluForecast/SideElements/EbolaRiskViewController.m
@@ -0,0 +1,37 @@
+//
+//  EbolaRiskViewController.m
+//  HHeal
+//
+//  Created by Changkun Zhao on 10/12/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import "EbolaRiskViewController.h"
+
+@interface EbolaRiskViewController ()
+
+@end
+
+@implementation EbolaRiskViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
