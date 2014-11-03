5ecfb07cb6c24196d9524ee0513a32ae43d2aa12
Changkun Zhao <changkunzhao@Changkuns-MacBook-Air.local>
2014-11-03 13:41:27 -0500

    updated to version 1.1. fixed some bugs, and add a new calculator.

---
+++ FluForecast/SideElements/HeartRateCalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  HeartRateCalculatorViewController.h
+//  HHeal
+//
+//  Created by Changkun Zhao on 10/31/14.
+//  Copyright (c) 2014 Changkun Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeartRateCalculatorViewController : UIViewController
+
+@end
