3482b6c10f1f69e7c0804915afcd3e5dc58367e1
Changkun Zhao <changkunzhao@Changkuns-Air.home>
2015-08-09 13:13:11 -0400

    -fixed watchUI -added training detail page

---
+++ HHeal WatchKit Extension/tableDetailInterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  tableDetailInterfaceController.h
+//  HHeal
+//
+//  Created by Changkun Zhao on 8/8/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface tableDetailInterfaceController : WKInterfaceController
+
+@end
---
+++ HHeal WatchKit Extension/tableDetailInterfaceController.m
@@ -0,0 +1,36 @@
+//
+//  tableDetailInterfaceController.m
+//  HHeal
+//
+//  Created by Changkun Zhao on 8/8/15.
+//  Copyright (c) 2015 Changkun Zhao. All rights reserved.
+//
+
+#import "tableDetailInterfaceController.h"
+
+@interface tableDetailInterfaceController ()
+
+@end
+
+@implementation tableDetailInterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+    
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
